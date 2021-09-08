b294459e49a4a9ed887c296272f47e7bc68b0c8a
RamsesEscoto <89435918+RamsesEscoto@users.noreply.github.com>
2021-09-07 20:45:56 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,7 +1,15 @@
 
 def main():
     #escribe tu código abajo de esta línea
-    pass
 
-if __name__=='__main__':
-    main()
+      num=int(input('Enter a number: '))
+    x=0
+    y=0
+    while y<num:
+        sum=x+b
+        x=1
+        b=sum
+        y=y+1
+    if y==num:
+        print(x)
+    pass
